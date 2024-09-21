80377739476c6caae61d0728f591bab4153c5b56
Sourasis Jana <sourasisjana6954@gmail.com>
2024-09-21 17:01:49 +0530

    Update Hotel_order.py

--- Hotel_order.py
+++ Hotel_order.py
@@ -1,40 +1,38 @@
 menu = {
-    "Pizza":50,
-    "Pasta":40,
-    "Burger":80,
-    "Momo":30,
-    "Coffee":70,
+    "Pizza": 50,
+    "Pasta": 40,
+    "Burger": 80,
+    "Momo": 30,
+    "Coffee": 70,
 }
 
 def add_item(menu, item, price):
     menu[item] = price
+
 add_item(menu, "Sandwich", 60)
 
-
-print("Welcom to JANA Restaurant")
-# print("Pizza: 50\nPasta: 40\nBurger: 80\nMomo: 30\nCoffee: 70")
+print("Welcome to JANA Restaurant")
 
 for item, price in menu.items():
     print(f"{item}: {price}")
 
+order_total = 0
 
-Order_total = 0
-
-item_1 = input("Enter the name of item you want to order =")
+item_1 = input("Enter the name of the item you want to order: ")
 if item_1 in menu:
-    Order_total = menu[item_1]
-    print(f"Your item {item_1} has been added to your order")
+    order_total = menu[item_1]
+    print(f"Your item '{item_1}' has been added to your order.")
     while True:
-        another_order = input("Do you want to add another item? (Yes/No) ").strip().lower()
-        if another_order == "Yes":
-            item_2 = input("Enter the name of second item = ")
+        another_order = input("Do you want to add another item? (Yes/No): ").strip().lower()
+        if another_order == "yes":
+            item_2 = input("Enter the name of the second item: ")
             if item_2 in menu:
-                Order_total += menu[item_2]
-                print(f"Item {item_2} has been added to order")
+                order_total += menu[item_2]
+                print(f"Item '{item_2}' has been added to your order.")
             else:
-                print(f"Ordered item {item_2}is not avaialable")
+                print(f"Ordered item '{item_2}' is not available.")
         else:
             break
-    print(f"The total amount of items is {Order_total}")
+    print(f"The total amount for your order is {order_total}")
 else:
-    print("Pleas order something else we can serve you")
+    print("Please order something else that we can serve you.")
