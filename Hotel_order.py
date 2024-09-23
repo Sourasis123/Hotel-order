8d6aa6f4670b3fc9ad2ccde48bc55259cdee27f9
Sourasis Jana <sourasisjana6954@gmail.com>
2024-09-23 12:29:18 +0530

    Update Hotel_order.py

--- Hotel_order.py
+++ Hotel_order.py
@@ -9,23 +9,29 @@ menu = {
 def add_item(menu, item, price):
     menu[item] = price
 
+def display_menu(menu):
+    for item, price in menu.items():
+        print(f"{item}: {price}")
+
+
 add_item(menu, "Sandwich", 60)
 
 print("Welcome to JANA Restaurant")
 
-for item, price in menu.items():
-    print(f"{item}: {price}")
+# for item, price in menu.items():
+#     print(f"{item}: {price}")
 
 order_total = 0
 
-item_1 = input("Enter the name of the item you want to order: ")
+item_1 = input("Enter the name of the item you want to order: ").strip().title()
 if item_1 in menu:
     order_total = menu[item_1]
     print(f"Your item '{item_1}' has been added to your order.")
+    
     while True:
-        another_order = input("Do you want to add another item? (Yes/No): ")
-        if another_order == "yes":
-            item_2 = input("Enter the name of the second item: ")
+        another_order = input("Do you want to add another item? (Yes/No): ").strip().capitalize()
+        if another_order == "Yes":
+            item_2 = input("Enter the name of the second item: ").strip().title()
             if item_2 in menu:
                 order_total += menu[item_2]
                 print(f"Item '{item_2}' has been added to your order.")
@@ -33,6 +39,7 @@ if item_1 in menu:
                 print(f"Ordered item '{item_2}' is not available.")
         else:
             break
+
     print(f"The total amount for your order is {order_total}")
 else:
     print("Please order something else that we can serve you.")
