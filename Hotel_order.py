690c6c84af0822f47ba69caa6320dc66cb2fcb00
Sourasis Jana <sourasisjana6954@gmail.com>
2024-09-25 00:48:15 +0530

    Update Hotel_order.py

--- Hotel_order.py
+++ Hotel_order.py
@@ -23,17 +23,19 @@ print("Welcome to JANA Restaurant")
 
 order_total = 0
 
-item_1 = input("Enter the name of the item you want to order: ").strip().title()
+item_1 = input("Enter the name of the item you want to order: ")
 if item_1 in menu:
     order_total = menu[item_1]
+    ordered_items = item_1
     print(f"Your item '{item_1}' has been added to your order.")
     
     while True:
-        another_order = input("Do you want to add another item? (Yes/No): ").strip().capitalize()
+        another_order = input("Do you want to add another item? (Yes/No): ")
         if another_order == "Yes":
-            item_2 = input("Enter the name of the second item: ").strip().title()
+            item_2 = input("Enter the name of the second item: ")
             if item_2 in menu:
                 order_total += menu[item_2]
+                ordered_items += " & " + item_2
                 print(f"Item '{item_2}' has been added to your order.")
             else:
                 print(f"Ordered item '{item_2}' is not available.")
